3e4af478797b3433347c77a10d602c0959342eca
DeepDiveDream <deepdivedream@gmail.com>
2022-10-13 19:38:33 +0700

    Add planned Event logging Comment unnecessary prints and write logs Delete old connection logs from DB

--- Event_TS_Checker.py
+++ Event_TS_Checker.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 import psycopg2
 import pyodbc
 import json
 from argparse import ArgumentParser
-from datetime import datetime
+from datetime import datetime, timedelta
 from psycopg2 import Error
 import sys
 import pandas as pd
@@ -11,25 +12,27 @@ from configparser import ConfigParser
 from pathlib import Path
 
 
-def connect_to_mssql(dbase, login, password, server):
+def connect_to_mssql(dbase, login, password, server, event_source):
     try:
         # conn_str = 'DRIVER={SQL Server}; SERVER=' + server + ';DATABASE=' + dbase + ';UID=' + login + ';PWD=' + password
         conn_str = 'Driver={ODBC Driver 18 for SQL Server}; SERVER=' + server + ';DATABASE=' + dbase + ';UID=' \
                    + login + '; PWD=' + password + ';TrustServerCertificate=yes;'
         conn = pyodbc.connect(conn_str)
 
-        msg = f"{datetime.now()} | {currentScript} | source:{event_type} |" \
+        msg = f"{datetime.now()} | {currentScript} | source: {event_source} |" \
             f" Соединение с БД {dbase}, сервер {server} успешно!"
-        print(msg)
-        write_log(log_file_name, msg)
+        # print(msg)
+        # write_log(log_file_name, msg)
+        write_connection_log(event_source, currentScript, server, dbase, True, msg)
 
         return conn
 
     except (Exception, Error) as connect_error:
-        local_error = f"{datetime.now()} | {currentScript} | source:{event_type} | " \
-            f"Ошибка соеденения с БД {dbase} сервер {server}: {connect_error}"
+        local_error = f"{datetime.now()} | {currentScript} | source: {event_source} | " \
+            f"Ошибка соединения с БД {dbase}, сервер {server}: {connect_error}"
         print(local_error)
         write_log(log_file_name, local_error)
+        write_connection_log(event_source, currentScript, server, dbase, False, local_error)
 
 
 def connect_to_postgre(dbase, login, password, host, port):
@@ -41,16 +44,16 @@ def connect_to_postgre(dbase, login, password, host, port):
             port=port,
             host=host)
 
-        msg = f"{datetime.now()} | {currentScript} | source:{event_type} |" \
-            f" Соединение с БД {dbase}, сервер {host} успешно!"
-        print(msg)
-        write_log(log_file_name, msg)
+        # msg = f"{datetime.now()} | {currentScript} | source: {event_source_main} |" \
+        #     f" Соединение с БД {dbase}, сервер {host} успешно!"
+        # print(msg)
+        # write_log(log_file_name, msg)
 
         return conn
 
     except (Exception, Error) as connect_error:
-        local_error = f'{datetime.now()} | {currentScript} | source:{event_type} | ' \
-            f'Ошибка соеденения с БД {dbase} сервер {host}: {connect_error}'
+        local_error = f'{datetime.now()} | {currentScript} | source: {event_source_main} | ' \
+            f'Ошибка соединения с БД {dbase}, сервер {host}: {connect_error}'
         print(local_error)
         write_log(log_file_name, local_error)
 
@@ -66,6 +69,22 @@ def fields(cursor):
     return results
 
 
+def write_connection_log(source_id, script_name, host, db_name, result, message):
+    message = message.replace("'", " ")
+    query = f"INSERT INTO  connection_log (source_id, script_name,host,db_name, date_time, result, message)" \
+        f" VALUES ({source_id}, '{script_name}' ,'{host}','{db_name}','{datetime.now()}', {result},'{message}')"
+    postgre_elsec_cursor.execute(query)
+    postgre_elsec_conn.commit()
+
+
+def delete_old_connection_log():
+    old_date = datetime.now() - timedelta(days=30)
+
+    query = f"DELETE FROM  connection_log WHERE date_time < timestamp '{old_date}'"
+    postgre_elsec_cursor.execute(query)
+    postgre_elsec_conn.commit()
+
+
 def write_log(file_name, str_log):
     with open(file_name, 'a') as logfile:
         logfile.write(str_log+'\n')
@@ -79,7 +98,7 @@ def check_event_source(event_source, param_dict):
     password = param_dict["password_scada_events"]
     dbase_scada_events = param_dict["db_scada_events"]
 
-    scada_events_conn = connect_to_mssql(dbase_scada_events, login, password, ip_address)
+    scada_events_conn = connect_to_mssql(dbase_scada_events, login, password, ip_address, event_source)
 
     if not scada_events_conn:
         return
@@ -97,7 +116,7 @@ def check_event_source(event_source, param_dict):
     password = param_dict["password_scada_model"]
     dbase_scada_model = param_dict["db_scada_model"]
 
-    scada_model_conn = connect_to_mssql(dbase_scada_model, login, password, ip_address)
+    scada_model_conn = connect_to_mssql(dbase_scada_model, login, password, ip_address, event_source)
 
     if not scada_model_conn:
         return
@@ -110,7 +129,7 @@ def check_event_source(event_source, param_dict):
     password = param_dict["password_asu_reo"]
     dbase_asu_reo = param_dict["db_asu_reo"]
 
-    mssql_asu_reo_conn = connect_to_mssql(dbase_asu_reo, login, password, ip_address)
+    mssql_asu_reo_conn = connect_to_mssql(dbase_asu_reo, login, password, ip_address, event_source)
 
     if not mssql_asu_reo_conn:
         return
@@ -123,55 +142,60 @@ def check_event_source(event_source, param_dict):
 
     #  загружаем данные из экселя от сапа
     excel_sap_file = sap_dir + param_dict["sap_file"]
-    # excel_sap_file =  param_dict["sap_file_path"]
+    # excel_sap_file =  param_dict["sap_file"]
     # Load spreadsheet
 
     path = Path(excel_sap_file)
     if not path.is_file():
-        msg = f'{datetime.now()} | {currentScript} | source:{event_type} | ' \
-            f'Файл {excel_sap_file} с данными SUP-R3 не найден!'
+        msg = f'{datetime.now()} | {currentScript} | source: {event_source} | ' \
+            f'Файл {excel_sap_file} с данными SAP-R3 не найден!'
         print(msg)
         write_log(log_file_name, msg)
+        write_connection_log(event_source, currentScript, "SAP", excel_sap_file, False, msg)
         return
 
     try:
         tech_places = pd.read_excel(excel_sap_file, usecols='F,H,I,J,K')
-        msg = f'{datetime.now()} | {currentScript} | source:{event_type} | ' \
-            f'Файл {excel_sap_file} с данными SUP-R3 успешно прочитан!'
+        msg = f'{datetime.now()} | {currentScript} | source: {event_source} | ' \
+            f'Файл {excel_sap_file} с данными SAP-R3 успешно прочитан!'
 
-        print(msg)
-        write_log(log_file_name, msg)
+        # print(msg)
+        # write_log(log_file_name, msg)
+        write_connection_log(event_source, currentScript, "SAP", excel_sap_file, True, msg)
 
     except (Exception, Error) as open_xls_error:
 
-        msg = f'{datetime.now()} | {currentScript} | source:{event_type} | ' \
-            f'Ошибка открытия файла {excel_sap_file} с данными SUP-R3: {open_xls_error}'
+        msg = f'{datetime.now()} | {currentScript} | source: {event_source} | ' \
+            f'Ошибка открытия файла {excel_sap_file} с данными SAP-R3: {open_xls_error}'
 
         print(msg)
         write_log(log_file_name, msg)
+        write_connection_log(event_source, currentScript, "SAP", excel_sap_file, False, msg)
         return
 
     query = f"SELECT scada_codes, description, scada_table " \
-        f"FROM scada_event_codes where elsec_source = {event_source} " \
+        f"FROM scada_event_codes where event_source = {event_source} " \
         f" and scada_table = \'{scada_event_table}\'"
 
     postgre_elsec_cursor.execute(query)
     res = postgre_elsec_cursor.fetchone()
 
     if res is None:
-        msg = f'{datetime.now()} | {currentScript} | source:{event_type} | ' \
+        msg = f'{datetime.now()} | {currentScript} | source: {event_source} | ' \
             f'Не удалось считать коды событий для таблицы "{res[1]}" [{res[2]}]'
         print(msg)
         write_log(log_file_name, msg)
+        write_connection_log(event_source_main, currentScript, postgre_host, postgre_database, False, msg)
         return
 
     event_codes = res[0]
 
     if not event_codes:
-        msg = f'{datetime.now()} | {currentScript} | source:{event_type} | ' \
+        msg = f'{datetime.now()} | {currentScript} | source: {event_source} | ' \
             f'Не заданы коды событий для таблицы "{res[1]} [{res[2]}]"'
         print(msg)
         write_log(log_file_name, msg)
+        write_connection_log(event_source_main, currentScript, postgre_host, postgre_database, False, msg)
         return
 
     for event_code in event_codes:
@@ -196,11 +220,11 @@ def check_event_source(event_source, param_dict):
 
         scada_events_cursor.execute(query)
         all_events = scada_events_cursor.fetchall()
-        msg = f'{datetime.now()} | {currentScript} | source:{event_type} | ' \
-            f'Найдено {len(all_events)} новых событий с кодом {event_code} в БД Скада: {dbase_scada_events}'
 
-        print(msg)
-        write_log(log_file_name, msg)
+        # msg = f'{datetime.now()} | {currentScript} | source: {event_source} | ' \
+        #     f'Найдено {len(all_events)} новых событий с кодом {event_code} в БД Скада: {dbase_scada_events}'
+        # print(msg)
+        # write_log(log_file_name, msg)
 
         query = f"Select nameLoc FROM [{dbase_scada_model}].[dbo].[EventCod] where code = {event_code} "
         scada_model_cursor.execute(query)
@@ -225,7 +249,8 @@ def check_event_source(event_source, param_dict):
 
             if user_id is not None:
                 # Получаем Юзера создавшего события
-                query = f"Select login_, firstName, midlName, lastName, department, post FROM [{dbase_scada_model}].[dbo].[Users] where KeyLink = '{user_id}'"
+                query = f"Select login_, firstName, midlName, lastName, department, post " \
+                        f"FROM [{dbase_scada_model}].[dbo].[Users] where KeyLink = '{user_id}'"
                 scada_model_cursor.execute(query)
                 res_user = scada_model_cursor.fetchone()
 
@@ -234,12 +259,13 @@ def check_event_source(event_source, param_dict):
                         "user_KeyLink": user_id,
                         "user_short": res_user[0]
                     }
-                    user_full_name = f"{res_user[1]} {res_user[2]} {res_user[3]}"
+                    user_full_name = f"{res_user[1]} {res_user[2]} {res_user[3]} {res_user[4]}"
                     user_values_string = f"<b>Пользователь:</b> , {user_full_name}, " \
                         f"{res_user[4]}, {res_user[5]}<br>"
 
             # Получаем Оборудование по которому создано событие
-            query = f"Select name_ FROM [{dbase_scada_model}].[dbo].[IdentifiedObject] where KeyLink = '{scada_event_obj}'"
+            query = f"Select name_ FROM [{dbase_scada_model}].[dbo].[IdentifiedObject] " \
+                    f"where KeyLink = '{scada_event_obj}'"
             scada_model_cursor.execute(query)
             query_result = scada_model_cursor.fetchone()
 
@@ -250,7 +276,8 @@ def check_event_source(event_source, param_dict):
 
             # получаем ИД оборудования по этой заявке в АСУ РЭО
             scada_model_cursor.execute(
-                f"SELECT top 1 AsureoDevId FROM [{dbase_scada_model}].[dbo].[LinkAsureo] where KeyLink = '{scada_event_obj}'")
+                f"SELECT top 1 AsureoDevId FROM [{dbase_scada_model}].[dbo].[LinkAsureo] "
+                f"where KeyLink = '{scada_event_obj}'")
 
             asureo_dev_id = scada_model_cursor.fetchone()
 
@@ -258,7 +285,8 @@ def check_event_source(event_source, param_dict):
             event_source_host = ''
 
             if user_id is not None:
-                query = f"Select top 1 caption FROM [{dbase_scada_events}].[dbo].[EvSys] where user_ = '{user_id}' and code = 1103 order by dt desc"
+                query = f"Select top 1 caption FROM [{dbase_scada_events}].[dbo].[EvSys] " \
+                        f"where user_ = '{user_id}' and code = 1103 order by dt desc"
                 scada_events_cursor.execute(query)
                 caption = scada_events_cursor.fetchone()[0]
 
@@ -294,7 +322,8 @@ def check_event_source(event_source, param_dict):
             if unplanned_event:
 
                 scada_model_cursor.execute(
-                    f"SELECT top 1 sapTM FROM [{dbase_scada_model}].[dbo].[LinkSAP] where KeyLink = '{scada_event_obj}'")
+                    f"SELECT top 1 sapTM FROM [{dbase_scada_model}].[dbo].[LinkSAP] "
+                    f"where KeyLink = '{scada_event_obj}'")
 
                 sap_tech_place = scada_model_cursor.fetchone()
 
@@ -312,8 +341,8 @@ def check_event_source(event_source, param_dict):
 
             if unplanned_event:
                 result_string = f"{event_description}, код [{event_code}]<br>" \
-                                    f"<b>Фактическое исполнение:</b> {scada_event_datetime}<br>" \
-                                    f"<b>Оборудование:</b> {dev_type_name}<br>" \
+                                f"<b>Фактическое исполнение:</b> {scada_event_datetime}<br>" \
+                                f"<b>Оборудование:</b> {dev_type_name}<br>" \
                                 + user_values_string
 
                 json_data = {
@@ -325,7 +354,7 @@ def check_event_source(event_source, param_dict):
                     "event_host": event_source_host,
                     "event_date": f'{scada_event_datetime}',
                     "event_code": event_code,
-                    "event_equipment" : dev_type_name,
+                    "event_equipment": dev_type_name,
                     "user_name": user_full_name
                 }
 
@@ -343,6 +372,31 @@ def check_event_source(event_source, param_dict):
                     data = json.dumps(json_data)
                     postgre_elsec_cursor.callproc('event_new', [event_type_blockip, event_source, False, data])
                     postgre_elsec_conn.commit()
+            else:
+                postgre_elsec_cursor.execute(f"SELECT * FROM event_type where name = \'plannedEvent\'")
+                planned_event_type = postgre_elsec_cursor.fetchone()[0]
+
+                result_string = f"{event_description}, код [{event_code}]<br>" \
+                                f"<b>Фактическое исполнение:</b> {scada_event_datetime}<br>" \
+                                f"<b>Оборудование:</b> {dev_type_name}<br>" \
+                                + user_values_string
+
+                json_data = {
+                    "data": user_json_data,
+                    "display": {
+                        "Событие запланировано. ": result_string
+                    },
+                    "event_description": event_description,
+                    "event_host": event_source_host,
+                    "event_date": f'{scada_event_datetime}',
+                    "event_code": event_code,
+                    "event_equipment": dev_type_name,
+                    "user_name": user_full_name
+                }
+
+                data = json.dumps(json_data)
+                postgre_elsec_cursor.callproc('event_new', [planned_event_type, event_source, True, data])
+                postgre_elsec_conn.commit()
 
             if last_request_unix_time <= 0:
                 query = f"INSERT INTO  scada_last_request (date, unix_date, " \
@@ -371,23 +425,27 @@ def check_event_source(event_source, param_dict):
 def check_all_event_sources():
 
         for param in event_source_params:
+            event_source = 0
             try:
                 event_source = param[fields_map_params['id']]
                 param_dict = param[fields_map_params['params']]
 
                 if len(param_dict) == 0:
-                    local_error = f'{datetime.now()} | {currentScript} | source:{event_type} ' \
+                    local_error = f'{datetime.now()} | {currentScript} | source: {event_source} ' \
                                 f'| Не заполнены параметры для источника "{event_source_name}"'
                     print(local_error)
                     write_log(log_file_name, local_error)
+                    write_connection_log(event_source, currentScript,
+                                         postgre_host, postgre_database, False, local_error)
                     continue
 
                 check_event_source(event_source, param_dict)
 
             except (Exception, Error) as error:
-                local_error = f'{datetime.now()} | {currentScript} | source:{event_type} | Ошибка: {error}'
+                local_error = f'{datetime.now()} | {currentScript} | source: {event_source} | Ошибка: {error}'
                 print(local_error)
                 write_log(log_file_name, local_error)
+                write_connection_log(event_source, currentScript, postgre_host, postgre_database, False, local_error)
                 continue
 
 
@@ -397,10 +455,12 @@ if __name__ == "__main__":
     scada_event_table = 'EvTs'
     event_type_name = 'unplannedTS'
     event_source_name = "telescada"
-    event_type = 0
+    event_source_main = 0
     log_file_name = None
     postgre_elsec_cursor = None
     postgre_elsec_conn = None
+    postgre_host = None
+    postgre_database = None
 
     try:
         parser = ArgumentParser()
@@ -425,8 +485,8 @@ if __name__ == "__main__":
         sap_dir = config_ini.get('events_checker', 'sap_dir')
         log_file_name = config_ini.get('events_checker', 'logfile')
 
-        postgre_elsec_conn = connect_to_postgre(postgre_database, postgre_user, postgre_pass, postgre_host,
-                                                postgre_port)
+        postgre_elsec_conn = connect_to_postgre(postgre_database, postgre_user,
+                                                postgre_pass, postgre_host, postgre_port)
 
         if log_dir != "":
             log_file_name = log_dir + "/" + log_file_name
@@ -436,6 +496,11 @@ if __name__ == "__main__":
 
         postgre_elsec_cursor = postgre_elsec_conn.cursor()
 
+        result_msg = f"{datetime.now()} | {currentScript} | source: {event_source_main} |" \
+            f" Соединение с БД {postgre_database}, сервер {postgre_host} успешно!"
+
+        write_connection_log(event_source_main, currentScript, postgre_host, postgre_database, True, result_msg)
+
         postgre_elsec_cursor.execute(f"SELECT * FROM event_type where name = \'{event_type_name}\'")
         event_type = postgre_elsec_cursor.fetchone()[0]
 
@@ -444,19 +509,25 @@ if __name__ == "__main__":
         event_source_params = postgre_elsec_cursor.fetchall()
 
         if event_source_params is None or len(event_source_params) == 0:
-            str_error = f'{datetime.now()} | {currentScript} | source:{event_type} ' \
-                        f'| Не найдет источник событий "{event_source_name}"'
+            str_error = f'{datetime.now()} | {currentScript} | source: {event_source_main} ' \
+                        f'| Не найдет источник событий "{event_source_name}", ' \
+                        f'БД "{postgre_database}", сервер "{postgre_host}"'
             print(str_error)
-            write_log(log_file_name,str_error)
+            write_log(log_file_name, str_error)
+            write_connection_log(event_source_main, currentScript, postgre_host, postgre_database, True, str_error)
             sys.exit(1)
+
+        delete_old_connection_log()
 
         check_all_event_sources()
 
     except (Exception, Error) as main_error:
-        str_error = f"{datetime.now()} | {currentScript} | source:{event_type} | Ошибка: {main_error}"
+        str_error = f"{datetime.now()} | {currentScript} | source: {event_source_main} | Ошибка: {main_error}"
         print(str_error)
         if log_file_name:
             write_log(log_file_name, str_error)
+        if postgre_elsec_conn and postgre_elsec_cursor and postgre_host and postgre_database:
+            write_connection_log(event_source_main, currentScript, postgre_host, postgre_database, True, str_error)
 
     finally:
         if postgre_elsec_cursor:
@@ -464,6 +535,3 @@ if __name__ == "__main__":
 
         if postgre_elsec_conn:
             postgre_elsec_conn.close()
-
-
-
